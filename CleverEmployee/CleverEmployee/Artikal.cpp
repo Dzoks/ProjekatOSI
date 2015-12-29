b2b28cf5c82b3a203b92d08dcd8a23ccc6aabf48
NemanjaMaric <nemanja.maric.22@hotmail.com>
2015-12-29 14:38:16 +0100

    Dodani GET-eti i SET-eri u klasu Artikal

--- CleverEmployee/CleverEmployee/Artikal.cpp
+++ CleverEmployee/CleverEmployee/Artikal.cpp
@@ -10,3 +10,53 @@ Artikal::Artikal()
 Artikal::~Artikal()
 {
 }
+
+void Artikal::setSifra(int x)
+{
+	sifra = x;
+}
+
+void Artikal::setKolicina(int x)
+{
+	kolicina = x;
+}
+
+void Artikal::setNaziv(std::string ime)
+{
+	naziv = ime;
+}
+
+void Artikal::setOpis(std::string x)
+{
+	opis = x;
+}
+
+void Artikal::setCijena(double x)
+{
+	cijena = x;
+}
+
+int Artikal::getSifra()
+{
+	return sifra;
+}
+
+int Artikal::getKolicina()
+{
+	return kolicina;
+}
+
+std::string Artikal::getNaziv()
+{
+	return naziv;
+}
+
+std::string Artikal::getOpis()
+{
+	return opis;
+}
+
+double Artikal::getCijena()
+{
+	return cijena;
+}
